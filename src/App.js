31597d9a0bebfc5e81a06225a917eb25212bb779
Park-Yong-Jun <9898junjun2@gmail.com>
2020-09-09 17:52:40 +0900

    Init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="gray-background">
+        <img scr={logo} lat="logo"></img>
+        <h2>Let's develop maanagement system!</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
